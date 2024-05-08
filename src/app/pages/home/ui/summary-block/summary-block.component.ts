6e07b1ae8e2e6c8a3cb4d25f27eeae5ccaec0eca
Yoann DREVET <yoann.drevet0@laposte.net>
2024-05-08 12:11:54 +0200

    Stylized the dashboard

--- src/app/pages/home/ui/summary-block/summary-block.component.ts
+++ src/app/pages/home/ui/summary-block/summary-block.component.ts
@@ -3,7 +3,7 @@ import { Component, Input, numberAttribute } from '@angular/core';
 @Component({
   selector: 'app-summary-block',
   templateUrl: './summary-block.component.html',
-  styleUrl: './summary-block.component.scss'
+    styleUrl: './summary-block.component.scss'
 })
 export class SummaryBlockComponent {
   @Input({required: true}) label!: string;
