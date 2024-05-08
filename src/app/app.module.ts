ca77e66db6c6f93c3ae7b4dd0ca7b8f135b0f3b7
Yoann DREVET <yoann.drevet0@laposte.net>
2024-05-08 11:11:55 +0200

    Moved statistics display in their own component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,9 +7,10 @@ import { HomeComponent } from './pages/home/home.component';
 import { NotFoundComponent } from './pages/not-found/not-found.component';
 import { NgxChartsModule } from '@swimlane/ngx-charts';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { SummaryBlockComponent } from './pages/home/ui/summary-block/summary-block.component';
 
 @NgModule({
-  declarations: [AppComponent, HomeComponent, NotFoundComponent],
+  declarations: [AppComponent, HomeComponent, NotFoundComponent, SummaryBlockComponent],
   imports: [BrowserModule, AppRoutingModule, HttpClientModule, NgxChartsModule, FontAwesomeModule],
   providers: [],
   bootstrap: [AppComponent],
---
+++ src/app/pages/home/ui/summary-block/summary-block.component.ts
@@ -0,0 +1,11 @@
+import { Component, Input, numberAttribute } from '@angular/core';
+
+@Component({
+  selector: 'app-summary-block',
+  templateUrl: './summary-block.component.html',
+  styleUrl: './summary-block.component.scss'
+})
+export class SummaryBlockComponent {
+  @Input({required: true}) label!: string;
+  @Input({required: true, transform: numberAttribute}) value!: number;
+}
