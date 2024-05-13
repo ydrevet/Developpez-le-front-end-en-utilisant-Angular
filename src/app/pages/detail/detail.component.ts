2e470c9a7245d285a787e40815c399d253b50167
Yoann DREVET <yoann.drevet0@laposte.net>
2024-05-13 10:22:23 +0200

    Get country name from URL

--- src/app/pages/detail/detail.component.ts
+++ src/app/pages/detail/detail.component.ts
@@ -1,12 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { OlympicService } from '../../core/services/olympic.service';
+import { ActivatedRoute, Params } from '@angular/router';
+import { map, tap } from 'rxjs/operators';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-detail',
   templateUrl: './detail.component.html',
   styleUrl: './detail.component.scss'
 })
-export class DetailComponent {
-  constructor(private olympicService: OlympicService) {
+export class DetailComponent implements OnInit {
+
+  constructor(private activatedRoute: ActivatedRoute, private olympicService: OlympicService) {
+  }
+
+  protected countryName$!: Observable<string>;
+
+  ngOnInit(): void {
+    this.countryName$ = this.activatedRoute.params.pipe(map((params: Params) => params['country']));
   }
 }
